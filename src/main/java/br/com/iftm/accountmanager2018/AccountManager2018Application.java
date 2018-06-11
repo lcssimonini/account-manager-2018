a24da20e47a97fb0a26be8783cf3b2cb136d2b87
lcssimonini <lcs.simonini@gmail.com>
2018-06-11 20:13:37 -0300

    inicial

---
+++ src/main/java/br/com/iftm/accountmanager2018/AccountManager2018Application.java
@@ -0,0 +1,12 @@
+package br.com.iftm.accountmanager2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManager2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManager2018Application.class, args);
+	}
+}
